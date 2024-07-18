80a08b0a0920e50506973aea87e2e93a6a4c42af
davissontiago <davissontiagolf@gmail.com>
2024-07-18 20:48:51 -0300

    Upando atividade01

---
+++ Atividade01Variavel/Variavel.java
@@ -0,0 +1,39 @@
+package Atividade01Variavel;
+
+public class Variavel {
+    public static void main(String[] args) {
+        // Questão 01
+        String nome = "Dávisson Tiago";
+        System.out.println("Nome: "+nome);
+        // Questão 02
+        String nome2 = "Dávisson Lima";
+        String curso = "Computação";
+        System.out.println("Nome: "+nome2);
+        System.out.println("Curso: "+curso);
+        // Questão 03
+        String nome3 = "Dávisson Ferreira";
+        int idade = 20;
+        System.out.println("Nome: "+nome3);
+        System.out.println("Idade: "+idade);
+        // Questão 04
+        int a = 3;
+        int b = 5;
+        int resultado = (2*a)*(3*b);
+        System.out.println("Resultado: "+resultado);
+        // Questão 05
+        int num1 = 10;
+        int num2 = 5;
+        int num3 = 8;
+        int soma = num1+num2+num3;
+        System.out.println("Soma: "+soma);
+        // Questão 06
+        double salario = 1500;
+        double imposto = salario*0.20;
+        System.out.println("Imposto: "+imposto);
+        // Questão 07
+        double base = 3;
+        double altura = 5;
+        double area = (base*altura)/2;
+        System.out.println("Area: "+area);
+    }
+}
